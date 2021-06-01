e3f2006758deb4a088f865e87e043ed3946a8de8
benintheb <ben051011@gmail.com>
2021-06-02 03:02:17 +0900

    a

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello, Flask!'
+  return render_template('controller.html')
+
+@app.route('/sensor')
+def sensor():
+  os.system("cd controller && python3 sensor.py")
+  return "sensor activated"
+
+host_addr = "0.0.0.0"
+port_num = "8080"
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host=host_addr, port=port_num, debug=True)
---
+++ controller/sensor.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+sensor = 16
+
+isLightOn = False
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor,GPIO.IN)
+
+os.system("./light.sh off")
+
+print("IR Sensor Ready.....")
+print(" ")
+
+try: 
+   while True:
+      if GPIO.input(sensor):
+          print("Object Detected")
+          if isLightOn == False:
+              os.system("./light.sh on")
+              isLightOn = True
+          elif isLightOn == True:
+              os.system("./light.sh off")
+              isLightOn = False
+          while GPIO.input(sensor):
+              time.sleep(0.2)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
