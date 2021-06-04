d89e1f78127e916dbcb8602d6b4804442a3d6901
benintheb <ben051011@gmail.com>
2021-06-04 18:08:07 +0900

    functions finished

--- app.py
+++ app.py
@@ -1,24 +1,63 @@
 from flask import Flask, render_template
 import os
-import subprocess
-import signal
+import subprocess, signal
+import g
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('controller.html')
 
-@app.route('/sensoron')
-def sensorOn():
-  global sensor
-  sensor = subprocess.Popen(["python3", "./sensor.py"])
-  return render_template('controller.html')
+  return render_template('index.html')
 
-@app.route('/sensoroff')
-def sensorOff():
-  sensor.kill()
-  return render_template('controller.html')
+@app.route('/light/<status>')
+def lightStatus(status):
+  if (status == "on"):
+    print("lightOn", status)
+    os.system("./light.sh on")
+  elif (status == "off"):
+    print("lightOff", status)
+    os.system("./light.sh off")
+  return render_template('index.html')
+
+@app.route('/sensor/<status>')
+def sensorStatus(status):
+  if (status == "on"):
+    print("sensorOn", status)
+    global sensor
+    sensor = subprocess.Popen(["python3", "./sensor.py"])
+  elif (status == "off"):
+    print("sensorOff", status)
+    sensor.kill()
+  return render_template('index.html')
+
+@app.route('/color/<color>')
+def colorChange(color):
+  if (color == "silver"):
+    print("silver", color)
+    os.system("./light.sh color silver")
+  elif (color == "green"):
+    print("green", color)
+    os.system("./light.sh color green")
+  elif (color == "blue"):
+    print("blue", color)
+    os.system("./light.sh color blue")
+  elif (color == "yellow"):
+    print("yellow", color)
+    os.system("./light.sh color yellow")
+  elif (color == "red"):
+    print("red", color)
+    os.system("./light.sh color red")
+  elif (color == "magenta"):
+    print("magenta", color)
+    os.system("./light.sh color magenta")
+  return render_template('index.html')
+
+@app.route('/dim/<value>')
+def dimControl(value):
+  print("dimvalue: ", value)
+  os.system("./light.sh brightness " + value)
+  return render_template('index.html')
 
 host_addr = "0.0.0.0"
 port_num = "8080"
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,13 @@
 const sensorBtnOff = document.getElementById("sensorIconOff");
-
 const sensorBtnOn = document.getElementById("sensorIconOn");
+const lightBtnOff = document.getElementById("lightIconOff");
+const lightBtnOn = document.getElementById("lightIconOn");
+
+const dimControl = document.getElementById("rangeInput");
 
 sensorBtnOff.onclick = (e) => {
   e.preventDefault();
-  fetch("/sensoron");
+  fetch("/sensor/on");
 
   sensorBtnOff.classList.add("hidden");
   sensorBtnOn.classList.remove("hidden");
@@ -12,8 +15,28 @@ sensorBtnOff.onclick = (e) => {
 
 sensorBtnOn.onclick = (e) => {
   e.preventDefault();
-  fetch("/sensoroff");
+  fetch("/sensor/off");
 
   sensorBtnOn.classList.add("hidden");
   sensorBtnOff.classList.remove("hidden");
+};
+
+lightBtnOff.onclick = (e) => {
+  e.preventDefault();
+  fetch("/light/on");
+
+  lightBtnOff.classList.add("hidden");
+  lightBtnOn.classList.remove("hidden");
+};
+
+lightBtnOn.onclick = (e) => {
+  e.preventDefault();
+  fetch("/light/off");
+
+  lightBtnOn.classList.add("hidden");
+  lightBtnOff.classList.remove("hidden");
+};
+
+dimControl.onchange = (e) => {
+  fetch("/dim/" + e.target.value);
 };
